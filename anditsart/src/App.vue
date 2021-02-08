4ee873c1a4a3deb6e3b1d8d8fa358ad03af933e5
Thomas Eykamp <embergameslp@gmail.com>
2021-02-08 00:47:06 -0500

    Moved images to JSON file

--- anditsart/src/App.vue
+++ anditsart/src/App.vue
@@ -25,13 +25,29 @@ export default {
   components: {
   ImageCards
   },
+  mounted(){
+    try {
+      this.fetchData();
+    } catch {
+      console.error("Cannot Fetch JSON Data");
+    }
+  },
+  methods: {
+    async fetchData(){
+      const res = await fetch("images.json");
+      const val = await res.json();
+      console.log(val);
+      this.images = val;
+    }
+  },
   data() {
     return {
-      images: [ // put art objects here
-        {id: 1, title: "google", address: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Photos_icon_%282020%29.svg/128px-Google_Photos_icon_%282020%29.svg.png"},
-        {id: 2, title: "microsoft", address: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31"}, 
-        {id: 3, title: "sensorbot", address: "http://sensorbot.org/img/sensorbot_logo_horiz_light.svg"}
-      ]
+      images: []
+      // images: [ // put art objects here
+      //   {id: 1, title: "google", address: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Photos_icon_%282020%29.svg/128px-Google_Photos_icon_%282020%29.svg.png"},
+      //   {id: 2, title: "microsoft", address: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31"}, 
+      //   {id: 3, title: "sensorbot", address: "http://sensorbot.org/img/sensorbot_logo_horiz_light.svg"}
+      // ]
     }
 
   }
