8a3aeff26174c73921c688f2b099ed32966dc3d9
Thomas Eykamp <embergameslp@gmail.com>
2021-02-12 04:13:50 -0500

    scrollToTop dynamically appears when 6+ members
    minor cosmetic cleanup

--- anditsart/src/App.vue
+++ anditsart/src/App.vue
@@ -6,7 +6,7 @@
     <div>
       <!-- left aligned nav items -->
       <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand href="#">anditsart</b-navbar-brand>
+        <b-navbar-brand class="anditsart">anditsart</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -19,7 +19,9 @@
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
-              <img class=searchIco src=https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png>
+              <b-navbar-brand>
+              <img class=searchIcon src=https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png>
+            </b-navbar-brand>
               <b-form-input size="sm" class="mr-sm-2" v-model="search" placeholder="Search"></b-form-input>
               <!-- <b-button size="sm" class="searchbtn" type="submit">Go!</b-button> -->
             </b-nav-form>
@@ -33,10 +35,11 @@
     <b-container fluid classs="format">
       <ImageCards v-bind:images="filteredImg"/>
     </b-container>
-    <button class="toTop" @click="scrollToTop()">
-      <i class="arrow up">
-      </i>
-    </button>
+    <div v-if="scrollTopUpdater">
+      <button class="toTop" @click="scrollToTop()">
+        <i class="arrow up"></i>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -50,36 +53,44 @@ export default {
   components: {
     ImageCards
   },
-  mounted(){
+  mounted() {
       this.fetchData();
   },
-  methods: {
+  methods: { // constant
     async fetchData() {
+      // read json
       try {
         const res = await fetch("images.json");
         const val = await res.json();
         console.log(val);
-        this.images = val;
+        this.images = val; // NEVER UPDATES
       } catch {
         console.error("Cannot Fetch JSON Data");
       }
     },
 
     scrollToTop() {
+      // scroll button
       window.scrollTo(0,0);
     }
   },
   data() {
     return {
       images: [], // imports from ./public/images.json
+      // NEVER UPDATES!!!!!!!!!!!!! --> use filteredImg for dynamic image list
       search: ""
     }
   },
-  computed: {
+  computed: { // updates
+    // search
     filteredImg: function() {
       return this.images.filter((image) => {
         return image.author.match(this.search) || image.title.match(this.search);
       });
+    },
+
+    scrollTopUpdater: function() {
+      return Object.keys(this.filteredImg).length > 6;
     }
 
   }
@@ -115,10 +126,14 @@ export default {
   padding: 10px;
   background: none;
 }
-.searchIco {
+.searchIcon {
   width: 30px;
   height: 30px;
-  margin-right: 20px;
+  transition-duration: 200ms;
+  
+}
+.searchIcon:hover {
+  transform: scale(1.5);
 }
 
 </style>
